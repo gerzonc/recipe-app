3d23023edd717e716e96fcffe09afdb9728ae5aa
Gerzon Zorrilla <gerzonc@icloud.com>
2023-03-24 01:56:28 -0400

    chore: create initial project folder structure

---
+++ client/src/screens/recipe-detail-screen/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const RecipeDetailScreen = () => {
+  return (
+    <View>
+      <Text>RecipeDetailScreen</Text>
+    </View>
+  );
+};
+
+export default RecipeDetailScreen;
---
+++ client/src/screens/recipe-list-screen/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const RecipeListScreen = () => {
+  return (
+    <View>
+      <Text>RecipeListScreen</Text>
+    </View>
+  );
+};
+
+export default RecipeListScreen;
