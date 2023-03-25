8e98e3ecc27bade0ecb3cbc51f2e6968f2c3e4d5
Gerzon <gerzonc@icloud.com>
2023-03-25 01:27:27 -0400

    add: react-native-context-menu-view (#6)

---
+++ client/src/screens/recipe-list-screen/context-menu/index.ts
@@ -0,0 +1,15 @@
+export const menuItems = [
+  {
+    title: "View",
+    systemIcon: "eye",
+  },
+  {
+    title: "Edit",
+    systemIcon: "square.and.pencil",
+  },
+  {
+    title: "Delete",
+    systemIcon: "trash",
+    destructive: true,
+  },
+];
