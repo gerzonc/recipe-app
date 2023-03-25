48becf7d47c1c00b4c89144c869ed82cf43b7ddd
Gerzon Zorrilla <gerzonc@icloud.com>
2023-03-25 05:16:27 -0400

    update: icon-button component

--- client/src/components/icon-button/index.tsx
+++ client/src/components/icon-button/index.tsx
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  View,
-  TouchableOpacity,
-  StyleSheet,
-  Pressable,
-  PressableProps,
-} from "react-native";
+import { View, StyleSheet, Pressable, PressableProps } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 
 interface IIconButtonProps extends PressableProps {
@@ -16,7 +10,7 @@ const IconButton: React.FC<IIconButtonProps> = ({ name, onPress }) => {
   return (
     <Pressable onPress={onPress}>
       <View style={styles.container}>
-        <FontAwesome name={name} size={16} color="#BDC0BF" />
+        <FontAwesome name={name} size={18} color="#0D0E0B" />
       </View>
     </Pressable>
   );
@@ -26,9 +20,9 @@ export default IconButton;
 
 const styles = StyleSheet.create({
   container: {
-    width: 32,
-    height: 32,
-    borderRadius: 16,
+    width: 38,
+    height: 38,
+    borderRadius: 8,
     backgroundColor: "#F4F5F6",
     justifyContent: "center",
     alignItems: "center",
